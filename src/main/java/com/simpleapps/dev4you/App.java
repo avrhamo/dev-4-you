02b63ccab964b3603ebb79e48f84cdd7172b417c
avrhamo <avrhamo@gmail.com>
2024-03-29 00:02:53 +0200

    init

--- src/main/java/com/simpleapps/dev4you/App.java
+++ src/main/java/com/simpleapps/dev4you/App.java
@@ -12,7 +12,6 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("app-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
